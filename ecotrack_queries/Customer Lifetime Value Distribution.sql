1b4157f5cc9cf9be5338dfea133e52e03a81eafe
Chris <157814013+cbonnin88@users.noreply.github.com>
2026-07-21 18:11:34 +0200

    Update Customer Lifetime Value Distribution.sql

--- ecotrack_queries/Customer Lifetime Value Distribution.sql
+++ ecotrack_queries/Customer Lifetime Value Distribution.sql
@@ -5,7 +5,7 @@ SELECT
     WHEN user_age BETWEEN 18 AND 25 THEN '18-25'
     WHEN user_age BETWEEN 26 AND 40 THEN '26-40'
     WHEN user_age BETWEEN 41 AND 50 THEN '41-50'
-    WHEN user_age BETWEEN 50 AND 60 THEN '50-60'
+    WHEN user_age BETWEEN 51 AND 60 THEN '50-60'
     ELSE '60+'
   END AS age_group,
   AVG(cumulative_user_ltv_eur) AS avg_ltv  
